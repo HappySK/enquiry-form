4f25b98444ff79c533cfbb41b58abb61dca37ef7
HappySK <shravanyoserene1729@gmail.com>
2021-05-01 00:05:56 +0530

    The Form Field is constructed

--- client-side/src/Components/EnquiryForm.js
+++ client-side/src/Components/EnquiryForm.js
@@ -1,18 +1,42 @@
-import { TextField } from "@material-ui/core";
+import { useState } from 'react'
+import { Button, TextField } from "@material-ui/core";
 import  axios  from 'axios'
+
 
 export const EnquiryForm = () => {
   
-  const getRes = () => {
-    axios.get('/send').then(res => {
-      console.log(res)
-    })
-  }
+  const [response, setResponse] = useState()
+  const [state, setState] = useState()
+  const [details, setDetails] = useState()
   
+  const handleChange = (e) => {
+      var { name, value } = e.target
+      setDetails({
+        ...details,
+        [name] : value
+      }, console.log(details))
+  }
+
   return(
     <div>
       <h1>Hello World</h1>
-      <button onClick={getRes}>Get Response from server</button>
+      <form>
+        <div>
+          <TextField label="Name" variant="outlined" name="Name" onBlur={handleChange}/>
+        </div>
+        <div>
+          <TextField label="Email ID" variant="outlined" name="EmailID" onBlur={handleChange}/>
+        </div>
+        <div>
+          <TextField label="Mobile Number" variant="outlined" name="MobileNo" onBlur={handleChange}/>
+        </div>
+        <div>
+          <TextField label="FeedBack" name="Feedback" variant="outlined" onBlur={handleChange}/>
+        </div>
+        <div>
+          <Button variant="contained">Submit</Button>
+        </div>
+      </form>
     </div>
   )
 }
