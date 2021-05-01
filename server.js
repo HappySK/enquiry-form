7d65699bac9aa74b5de940e4b43b2fd92a96e71a
HappySK <shravanyoserene1729@gmail.com>
2021-05-02 00:58:04 +0530

    Included Body parser in order to recieve data from the request

--- server.js
+++ server.js
@@ -2,9 +2,11 @@ const express = require('express')
 const cors = require('cors')
 const app = express()
 app.use(cors())
+app.use(express.json())
 
-app.get('/send',(req, res) => {
-  res.send('From Express Server')
+app.post('/send',(req, res) => {
+  console.log(req.body)
+  res.send('Form Submitted successfully')
 })
 
 app.listen(5000,() => {
